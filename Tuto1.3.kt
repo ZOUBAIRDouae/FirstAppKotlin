eaf2a718954bfc2be0f82ae9511887233040ed61
ZOUBAIRDouae <zoubairdouae5@gmail.com>
2024-10-08 16:13:17 +0100

    update6

--- Tuto1.3.kt
+++ Tuto1.3.kt
@@ -7,13 +7,16 @@ fun birthdayGreeting() {
     println("You are now 5 years old!")
 }*/
 
-fun main() {
-    println(birthdayGreeting("Rover", 5))
-    println(birthdayGreeting("Rex", 2))
-}
+
 
 fun birthdayGreeting(name: String, age: Int): String {
     val nameGreeting = "Happy Birthday, $name!"
     val ageGreeting = "You are now $age years old!"
     return "$nameGreeting\n$ageGreeting"
+}
+
+
+fun main() {
+    println(birthdayGreeting("Rover", 5))
+    println(birthdayGreeting("Rex", 2))
 }
